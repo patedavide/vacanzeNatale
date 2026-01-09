f3729a89d20874d3f3b27fd77094dcb304738c8e
paterno.22139 <paterno.davide.studente@itispaleocapa.it>
2026-01-09 12:43:51 +0100

    Eliminazione file inutili

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         GestioneFileCSV gestione = new GestioneFileCSV(
                 "paterno.csv",   // CSV reale
-                "temp_cancellazione.csv",
+                "paterno_modificato.csv",
                 ","
         );
 
